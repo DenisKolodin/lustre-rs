d3d64852d143ad4bb1e0eb8b738164c7c3b4bb21
Nicolas Barrios <nbarrios1337@gmail.com>
2022-10-12 12:30:35 -0400

    Move ray scatter helpers to where they're used

--- src/material.rs
+++ src/material.rs
@@ -5,13 +5,27 @@ use std::{f32::EPSILON, sync::Arc};
 use glam::Vec3A;
 use rand::Rng;
 
-use crate::{
-    color::Color,
-    hittables::HitRecord,
-    ray::Ray,
-    scatter::{reflect, refract},
-    textures::Texture,
-};
+use crate::{color::Color, hittables::HitRecord, ray::Ray, textures::Texture};
+
+/// Returns a reflected ray direction based on the given normal
+///
+/// Performs the following computation: `v - 2 * v.dot(n) * n`
+#[inline]
+fn reflect(v: Vec3A, n: Vec3A) -> Vec3A {
+    v - n * v.dot(n) * 2.0
+}
+
+/// Returns a refracted ray direction using the given normal
+/// and the ratio between two refractive indices.
+///
+/// See [Shirley's RTiOW's section on Snell's Law](https://raytracing.github.io/books/RayTracingInOneWeekend.html#dielectrics/snell'slaw) for more information
+#[inline]
+fn refract(uv: Vec3A, n: Vec3A, eta_ratio: f32) -> Vec3A {
+    let cos_theta = (-uv).dot(n).min(1.0);
+    let r_perp = eta_ratio * (uv + cos_theta * n);
+    let r_para = (1.0 - r_perp.length_squared()).abs().sqrt() * -1.0 * n;
+    r_perp + r_para
+}
 
 /// Enumeration of possible material types.
 #[derive(Debug)]
