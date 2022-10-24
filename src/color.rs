5fc8d9b4ecf1c41e5667b69963b18b3af824811d
Nicolas Barrios <nbarrios1337@gmail.com>
2022-10-24 16:33:15 -0400

    use map to compute on arrays for color conversion

--- src/color.rs
+++ src/color.rs
@@ -18,16 +18,20 @@ impl Color {
 }
 
 impl From<Color> for Vec3A {
-    fn from(c: Color) -> Self {
-        c.value
+    fn from(color: Color) -> Self {
+        color.value
     }
 }
 
 // The important stuff
 impl From<Color> for image::Rgb<u8> {
     fn from(color: Color) -> Self {
-        let converted = color.value.clamp(Vec3A::ZERO, Vec3A::ONE) * 255.0;
-        Self([converted.x as u8, converted.y as u8, converted.z as u8])
+        Self(
+            color
+                .value
+                .to_array()
+                .map(|channel| (channel.clamp(0.0, 1.0) * 255.0) as u8),
+        )
     }
 }
 
@@ -35,11 +39,7 @@ impl From<image::Rgb<u8>> for Color {
     fn from(rgb: image::Rgb<u8>) -> Self {
         let scale = 1.0 / 255.0;
         Self {
-            value: Vec3A::from_array([
-                rgb[0] as f32 * scale,
-                rgb[1] as f32 * scale,
-                rgb[2] as f32 * scale,
-            ]),
+            value: Vec3A::from_array(rgb.0.map(|channel| channel as f32 * scale)),
         }
     }
 }
