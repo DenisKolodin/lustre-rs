54fffcd594890d6d392563d7073180b5b545fc89
Nicolas Barrios <nbarrios1337@gmail.com>
2022-11-18 11:18:53 -0500

    Add unit tests for rand helpers

--- src/utils/random.rs
+++ src/utils/random.rs
@@ -31,3 +31,30 @@ pub fn rand_vec3_in_unit_disk(rng: &mut impl Rng) -> Vec3A {
     let [x, y] = UnitDisc.sample(rng);
     Vec3A::new(x, y, 0.0)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rand_unit_sphere() {
+        let mut rng = rand::thread_rng();
+        let res = rand_vec3_on_unit_sphere(&mut rng);
+        assert!(
+            res.is_normalized(),
+            "the unit vector {res}'s length was {}",
+            res.length()
+        )
+    }
+
+    #[test]
+    fn test_rand_unit_disk() {
+        let mut rng = rand::thread_rng();
+        let res = rand_vec3_in_unit_disk(&mut rng);
+        assert!(
+            res.length() <= 1.0,
+            "expected a vector with a length <= 1, found a vector {res} with length {}",
+            res.length()
+        )
+    }
+}
