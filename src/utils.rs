4eb71241327d40f5aab22d54cc433f8168d8a30c
Nicolas Barrios <nbarrios1337@gmail.com>
2022-12-13 13:50:02 -0500

    Add newtype-like enum for axis determination
    supports indexing into Vec3A and conversion to Vec3A

--- src/utils.rs
+++ src/utils.rs
@@ -6,3 +6,36 @@ pub mod arena;
 pub mod match_opts;
 pub mod progress;
 pub mod random;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Axis {
+    X,
+    Y,
+    Z,
+}
+
+impl Axis {
+    pub const AXES: [Axis; 3] = [Axis::X, Axis::Y, Axis::Z];
+}
+
+impl std::ops::Index<Axis> for glam::Vec3A {
+    type Output = f32;
+
+    #[inline]
+    fn index(&self, index: Axis) -> &Self::Output {
+        &self[index as usize]
+    }
+}
+
+impl std::ops::IndexMut<Axis> for glam::Vec3A {
+    #[inline]
+    fn index_mut(&mut self, index: Axis) -> &mut Self::Output {
+        &mut self[index as usize]
+    }
+}
+
+impl From<Axis> for glam::Vec3A {
+    fn from(axis: Axis) -> Self {
+        glam::Vec3A::AXES[axis as usize]
+    }
+}
