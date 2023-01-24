0f9724dad517badcebcf26b1ef53de9f33b8135d
Nicolas Barrios <nbarrios1337@gmail.com>
2023-01-23 21:10:32 -0500

    Add tree intersection test benchmarking
    * need hittables module public for trait
    * benches assert that the ray will (not) hit the scene
    * default config okay due to relative lack of noise

--- benches/tree.rs
+++ benches/tree.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use lustre::{scenes, tree::Tree};
+use lustre::{hittables::Hittable, scenes, tree::Tree};
 use rand::SeedableRng;
 
 fn bench_gen(c: &mut Criterion) {
@@ -32,5 +32,75 @@ fn bench_gen(c: &mut Criterion) {
     bench_group.finish();
 }
 
-criterion_group! {benches, bench_gen}
+fn bench_hit(c: &mut Criterion) {
+    // configuration of criterion
+    let mut bench_group = c.benchmark_group("tree_intersect");
+    // more samples -> more precision
+    bench_group.sample_size(1000);
+
+    // modify scene selection here
+    let scenes_to_check = [scenes::SceneType::CoverPhoto];
+
+    // check against each chosen scene
+    for scene in scenes_to_check {
+        // tree configuration
+
+        let mut rng = rand::rngs::SmallRng::seed_from_u64(0);
+        let geo = scenes::get_geometry(scene, &mut rng, 0.0..1.0);
+        let tree = Tree::new(geo, 0.0, 1.0);
+
+        // ray configuration
+        // viewport@ (0.5,0.5) is center of the image. should be guaranteed to hit with specific scenes
+        let ray = lustre::scenes::get_camera(scene).get_ray(0.5, 0.5, &mut rng);
+
+        // correctness check
+        assert!(
+            tree.hit(&ray, 0.001, f32::INFINITY).is_some(),
+            "ray directed at the center of the scene should hit the scene"
+        );
+
+        // benchmark
+        let scene_name = format!("{scene:?}");
+        bench_group.bench_function(BenchmarkId::new("definite_hit", scene_name), |b| {
+            b.iter(|| tree.hit(&ray, 0.001, f32::INFINITY))
+        });
+    }
+}
+
+fn bench_miss(c: &mut Criterion) {
+    // configuration of criterion
+    let mut bench_group = c.benchmark_group("tree_intersect");
+    // more samples -> more precision
+    bench_group.sample_size(1000);
+
+    // modify scene selection here
+    let scenes_to_check = [scenes::SceneType::CoverPhoto];
+
+    // check against each chosen scene
+    for scene in scenes_to_check {
+        // tree configuration
+        let mut rng = rand::rngs::SmallRng::seed_from_u64(0);
+        let geo = scenes::get_geometry(scene, &mut rng, 0.0..1.0);
+        let tree = Tree::new(geo, 0.0, 1.0);
+
+        // ray configuration
+        let mut ray = lustre::scenes::get_camera(scene).get_ray(0.5, 0.5, &mut rng);
+        // negating all components "flips" the vector
+        ray.direction *= -1.0;
+
+        // correctness check
+        assert!(
+            tree.hit(&ray, 0.001, f32::INFINITY).is_none(),
+            "ray directed away from the scene should not hit the scene"
+        );
+
+        // benchmark
+        let scene_name = format!("{scene:?}");
+        bench_group.bench_function(BenchmarkId::new("definite_miss", scene_name), |b| {
+            b.iter(|| tree.hit(&ray, 0.001, f32::INFINITY))
+        });
+    }
+}
+
+criterion_group! {benches, bench_gen, bench_hit, bench_miss}
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,18 @@ mod bounds;
 mod bvh;
 mod camera;
 mod color;
-mod hittables;
 mod material;
 mod ray;
 mod textures;
 mod utils;
 
+// public: commandline parser
 pub mod cli;
+// public: `Hittable` trait
+pub mod hittables;
+// public: renderer functionality
 pub mod render;
+// public: scene selection
 pub mod scenes;
+// public: tree benchmarking
 pub mod tree;
